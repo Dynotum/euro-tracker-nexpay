98310223b026de9a9ff61709350e25ed774032d1
Dyno <4706304+Dynotum@users.noreply.github.com>
2021-12-31 10:29:44 -0600

    Update Constants.java

--- src/main/java/Constants.java
+++ src/main/java/Constants.java
@@ -2,8 +2,8 @@ public final class Constants {
     // Find your Account SID and Auth Token at twilio.com/console
     // and set the environment variables. See http://twil.io/secure
 
-    public static final String ACCOUNT_SID = System.getenv("TWILIO_ACCOUNT_SID"); //"AC8778744a9e5c10a01e6df7e9a83ef58f";
-    public static final String AUTH_TOKEN = System.getenv("TWILIO_AUTH_TOKEN"); // "d07429ebc7d03aa2d7745b5b0f0d17b0";
+    public static final String ACCOUNT_SID = System.getenv("TWILIO_ACCOUNT_SID"); 
+    public static final String AUTH_TOKEN = System.getenv("TWILIO_AUTH_TOKEN");
 
     public static final String STEPH_NUMBER = System.getenv("STEPH_NUMBER");
     public static final String ME_NUMBER = System.getenv("ME_NUMBER");
